ffc52d903c3211f85a1f32f7c02af9ea1f4dab3f
sophie-tunecore <sophie.debenedetto@gmail.com>
2018-04-20 15:21:03 -0400

    start seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,5 +7,20 @@
 #
 #     PhoenixPair.Repo.insert!(%PhoenixPair.SomeModel{})
 #
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+user_data =   %{
+                first_name: "moebi",
+                last_name: "huang",
+                email: "moebi@email.com",
+                password: "password",
+                password_confirmation: "password"
+              }
+changeset = PhoenixPair.User.changeset(%PhoenixPair.User{}, user_data)
+PhoenixPair.Repo.insert!(changeset)
+
+challenge_data   = %{
+                      title: "Multiples of 3 and 5",
+                      prompt: "If we take all of the natural numbers below 10 that are multiples of 3 and 5, we get 3, 5, 6, and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 and 5 below 1000."
+                    }
+changeset        = PhoenixPair.Challenge.changeset(%PhoenixPair.Challenge, challange_data)
+{:ok, challenge} = PhoenixPair.Repo.insert!(changeset)
+
